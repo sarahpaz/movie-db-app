5cadddb1ff28b1ba642335f4d55fcf676c9d81eb
Sarah Paz <sarah.paz@gmail.com>
2020-01-17 22:54:31 -0500

    add return to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,15 +14,23 @@ import MovieThumb from "./elements/MovieThumb";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
 
-const Home = () => (
-  <>
-    <HeroImage />
-    <SearchBar />
-    <Grid />
-    <MovieThumb />
-    <Spinner />
-    <LoadMoreBtn />
-  </>
-);
+// custom hook
+import { useHomeFetch } from "./hooks/useHomeFetch";
+
+const Home = () => {
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  console.log(state);
+
+  return (
+    <>
+      <HeroImage />
+      <SearchBar />
+      <Grid />
+      <MovieThumb />
+      <Spinner />
+      <LoadMoreBtn />
+    </>
+  );
+};
 
 export default Home;
