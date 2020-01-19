7de83473082b4ce941a230412c6e764fc9d9d6fe
Sarah Paz <sarah.paz@gmail.com>
2020-01-18 20:14:16 -0500

    create loadMoreMovies callback for endpoints

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,6 +30,17 @@ const Home = () => {
   ] = useHomeFetch();
   const [searchTerm, setSearchTerm] = useState("");
 
+  const loadMoreMovies = () => {
+    const searchEndpoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage +
+      1}`;
+    const popularEndpoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=${currentPage +
+      1}`;
+
+    const endpoint = searchTerm ? searchEndpoint : popularEndpoint;
+
+    fetchMovies(endpoint);
+  };
+
   if (error) return <div>Something went wrong...</div>;
   if (!movies[0]) return <Spinner />;
 
@@ -56,9 +67,10 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <MovieThumb />
-      <Spinner />
-      <LoadMoreBtn />
+      {loading && <Spinner />}
+      {currentPage < totalPages && !loading && (
+        <LoadMoreBtn text="Load More" callback={loadMoreMovies} />
+      )}
     </>
   );
 };
