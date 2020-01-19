3558b6f08309d6ee3767deef42e8dc320246dba2
Sarah Paz <sarah.paz@gmail.com>
2020-01-18 19:35:12 -0500

    import GlobalStyle

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 // import components
 import Header from "./elements/Header";
 import Home from "./Home";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 const App = () => (
-  <div>
+  <>
     <Header />
     <Home />
-  </div>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
