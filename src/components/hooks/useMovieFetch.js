3f0e519993fb7e855dfd8d6af798930b52a430f0
Sarah Paz <sarah.paz@gmail.com>
2020-01-19 20:34:18 -0500

    setState for actors and directors

--- src/components/hooks/useMovieFetch.js
+++ src/components/hooks/useMovieFetch.js
@@ -13,10 +13,19 @@ export const useMovieFetch = movieId => {
     try {
       const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
       const result = await (await fetch(endpoint)).json();
-      console.log(result);
+      // console.log(result);
       const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
       const creditsResult = await (await fetch(creditsEndpoint)).json();
-      console.log(creditsResult);
+      // console.log(creditsResult);
+      const directors = creditsResult.crew.filter(
+        member => member.job === "Director"
+      );
+
+      setState({
+        ...result,
+        actors: creditsResult.cast,
+        directors
+      });
     } catch (error) {
       setError(true);
     }
