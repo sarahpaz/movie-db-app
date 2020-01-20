251e6f3060c426b645a46e4cac8ad8ec02615660
Sarah Paz <sarah.paz@gmail.com>
2020-01-19 22:57:57 -0500

    set up sessionStorage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,6 +20,7 @@ import { useHomeFetch } from "./hooks/useHomeFetch";
 import NoImage from "./images/no_image.jpg";
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [
     {
       state: { movies, currentPage, totalPages, heroImage },
@@ -27,8 +28,7 @@ const Home = () => {
       error
     },
     fetchMovies
-  ] = useHomeFetch();
-  const [searchTerm, setSearchTerm] = useState("");
+  ] = useHomeFetch(searchTerm);
 
   const searchMovies = search => {
     const endpoint = search ? SEARCH_BASE_URL + search : POPULAR_BASE_URL;
